7e33c91c3eb4e492baad0cd97333c30e6b338b49
GermanoBruno <bruno.germanon@gmail.com>
2021-05-17 19:09:13 -0300

    adicionado .c e .c dos arquivos

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int opNum;
+	printf("Qual operacao?\n");
+	scanf("%d", &opNum);
+
+	switch(opNum){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		default:
+			break;
+	}
+
+	return 0;
+}
