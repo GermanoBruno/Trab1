267810e3be21814bf9e96451c76e1123b955cef5
Victor Gomes <vi.tao1000@hotmail.com>
2021-05-22 21:50:20 -0300

    Novas implementações

--- veiculos.c
+++ veiculos.c
@@ -80,6 +80,17 @@ void setHeader(header* h, FILE* binario) {
 
 }
 
+void atualizaHeader (header* h, FILE* binario) {
+
+	fseek(binario, 0, SEEK_SET);
+	h->status = '1';
+	fwrite(&(h->status), sizeof(char), 1, binario);
+	fwrite(&(h->byteProxReg), sizeof(long int), 1, binario);
+	fwrite(&(h->nroRegistros), sizeof(int), 1, binario);
+	fwrite(&(h->nroRegistrosRemovidos), sizeof(int), 1, binario);
+
+}
+
 veiculo* create() {
 
 	veiculo *v = (veiculo*)malloc(sizeof(veiculo));
@@ -187,11 +198,12 @@ void atualizaBinario(header* h, veiculo* v, FILE* binario) {
 	h->byteProxReg = h->byteProxReg + tam;
 	//printf("%ld\n", h->byteProxReg);
 	
+	/*
 	fseek(binario, 1, SEEK_SET);
 	fwrite(&(h->byteProxReg), sizeof(long int), 1, binario);
 	fwrite(&(h->nroRegistros), sizeof(int), 1, binario);
 	fwrite(&(h->nroRegistrosRemovidos), sizeof(int), 1, binario);
-
+	*/
 }
 
 void imprime (veiculo* v) {
@@ -208,9 +220,100 @@ void imprime (veiculo* v) {
 
 void libera (veiculo* v) {
 
-	free(v->modelo);
-	free(v->categoria);
-	free(v);
+	if (v != NULL) {
+	
+		if (v->modelo != NULL) free(v->modelo);
+		if (v->categoria != NULL) free(v->categoria);
+		free(v);
+	
+	}
+}
+
+void busca (FILE* binario, char* campo, int n) {
+
+	/*	Prefixo do veiculo: BA004
+		Modelo do veiculo: NEOBUS MEGA
+		Categoria do veiculo: ALIMENTADOR
+		Data de entrada do veiculo na frota: 29 de maio de 2009
+		Quantidade de lugares sentados disponiveis: campo com valor nulo
+		--- pule uma linha em branco ---
+	*/
+
+	char removido, prefixo[6], data[11], *modelo, *categoria;
+	int tam, qtdlug, tamodelo, tamcategoria, codlinha, i = 0, fdata = 0;
+	fseek(binario, 175, SEEK_SET);
+
+	while (i < n) {
+		
+		fread(&(removido), sizeof(char), 1, binario);
+		if (removido == '0') {
+			
+			i++;
+			printf("Registro inexistente\n");
+			fread(&(tam), sizeof(int), 1, binario);
+			fseek(binario, tam, SEEK_CUR);
+			continue;
+		}  
+
+		fread(&(tam), sizeof(int), 1, binario);
+		//printf("Tamanho do registro: %d\n", tam);
+
+		fread(prefixo, sizeof(char), 5, binario);
+		prefixo[5] = '\0';
+		//printf("Prefixo do veiculo: %s\n", prefixo);
+
+		fread(data, sizeof(char), 10, binario);
+		data[10] = '\0';
+		if (data[0] == '@') fdata = 1;
+		//printf("Data de entrada do veiculo na frota: %s\n", data);
+
+		fread(&(qtdlug), sizeof(int), 1, binario);
+		//printf("Quantidade de lugares sentados disponiveis: %d\n", qtdlug);
+
+		fread(&(codlinha), sizeof(int), 1, binario);
+		//printf("Codigo da linha: %d\n", codlinha);
+
+		fread(&(tamodelo), sizeof(int), 1, binario);
+		if (tamodelo != 0) {
+			
+			modelo = (char*)malloc(sizeof(char) * tamodelo + 1); 
+			fread(modelo, sizeof(char), tamodelo, binario);
+			modelo[tamodelo] = '\0';
+		
+		}
+		//printf("Modelo do veiculo: %s\n", modelo);
+
+		fread(&(tamcategoria), sizeof(int), 1, binario);
+		categoria = (char*)malloc(sizeof(char) * tamcategoria + 1); 
+		fread(categoria, sizeof(char), tamcategoria, binario);
+		categoria[tamcategoria] = '\0';
+		//printf("Categoria do veiculo: %s\n", categoria);
+
+		if (strcmp(campo, prefixo) == 0) {
+
+			printf("Tamanho do registro: %d\n", tam);
+			printf("Prefixo do veiculo: %s\n", prefixo);
+			printf("Data de entrada do veiculo na frota: ");
+			if (fdata) printf("NULO\n");
+			else printf("%s\n", data);
+
+			printf("Quantidade de lugares sentados disponiveis: %d\n", qtdlug);
+			printf("Codigo da linha: %d\n", codlinha);
+
+			printf("Modelo do veiculo: ");
+			if (tamodelo) printf("%s\n", modelo);
+			else printf("NULO\n");
+			printf("Categoria do veiculo: %s\n", categoria);
+			printf("\n");
+
+		}
+
+		if (tamodelo) free(modelo);
+		free(categoria);
+
+		i++;
+
+	}
 
 }
 
