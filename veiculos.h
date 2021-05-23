9637e31b734b8ada2f4a2675d287d288558b4339
Victor Gomes <vi.tao1000@hotmail.com>
2021-05-22 21:50:55 -0300

    Novos cabeçalhos

--- veiculos.h
+++ veiculos.h
@@ -7,12 +7,14 @@ typedef struct Veiculo veiculo;
 header* createHeader();
 void descreveHeader(header* h, FILE* fp);
 void setHeader(header* h, FILE* binario);
+void atualizaHeader (header* h, FILE* binario);
 veiculo* create();
 int leitura (FILE* fp, veiculo* v);
 int escrita (veiculo* v, char removido, int byteProxReg, FILE* binario);
 void atualizaBinario(header* h, veiculo* v, FILE* binario);
 void imprime (veiculo* v);
 void libera (veiculo* v);
+void busca (FILE* binario, char* campo, int n);
 void binarioNaTela(char *nomeArquivoBinario);
 
 
